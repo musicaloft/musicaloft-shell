d2c845f6ea234a349f2aa1062bc62c8493ac151b
municorn <municorn@musicaloft.com>
2026-09-18 22:57:18 -0600

    refactor(languages/rust): wire crane and dioxus modules into rust module
    - split the module into an unconditional imports list and a config
      section gated by languages.rust.enable, so submodules can attach
      options regardless of whether rust is enabled
    - import ./crane.nix and ./dioxus.nix, exposing their options under
      languages.rust.crane and languages.rust.dioxus

--- modules/languages/rust/default.nix
+++ modules/languages/rust/default.nix
@@ -5,66 +5,74 @@
   ...
 }:
 let
-  toolchain = config.languages.rust.toolchainPackage;
+  cfg = config.languages.rust;
+  toolchain = cfg.toolchainPackage;
 in
-lib.mkIf config.languages.rust.enable {
-  files = {
-    ".cargo/config.toml".toml.alias.cover = "llvm-cov nextest";
-    ".cargo/mutants.toml".toml.test_tool = "nextest";
-    ".cargo/nextest.toml".toml.profile.ci = {
-      fail-fast = false;
-      retries = 4;
-    };
-    ".rustfmt.toml".toml = {
-      condense_wildcard_suffixes = true;
-      float_literal_trailing_zero = "IfNoPostfix";
-      format_code_in_doc_comments = true;
-      format_macro_bodies = true;
-      format_macro_matchers = true;
-      format_strings = true;
-      group_imports = "StdExternalCrate";
-      hex_literal_case = "Lower";
-      imports_granularity = "Crate";
-      normalize_comments = true;
-      normalize_doc_attributes = true;
-      overflow_delimited_expr = true;
-      reorder_impl_items = true;
-      style_edition = "2024";
-      unstable_features = true;
-      use_field_init_shorthand = true;
-      use_try_shorthand = true;
-      wrap_comments = true;
-    };
-  };
-
-  languages.rust.components = lib.mkOptionDefault [ "llvm-tools-preview" ];
-
-  git-hooks.hooks.clippy = {
-    enable = true;
-    packageOverrides = {
-      cargo = toolchain;
-      clippy = toolchain;
-    };
-    settings = {
-      allFeatures = true;
-      denyWarnings = true;
-    };
-  };
-
-  packages = with pkgs; [
-    bacon
-    cargo-outdated
-    cargo-llvm-cov
-    cargo-machete
-    cargo-mutants
-    cargo-nextest
+{
+  imports = [
+    ./crane.nix
+    ./dioxus.nix
   ];
 
-  treefmt.config.programs = {
-    rustfmt = {
-      enable = true;
-      package = toolchain;
+  config = lib.mkIf cfg.enable {
+    files = {
+      ".cargo/config.toml".toml.alias.cover = "llvm-cov nextest";
+      ".cargo/mutants.toml".toml.test_tool = "nextest";
+      ".cargo/nextest.toml".toml.profile.ci = {
+        fail-fast = false;
+        retries = 4;
+      };
+      ".rustfmt.toml".toml = {
+        condense_wildcard_suffixes = true;
+        float_literal_trailing_zero = "IfNoPostfix";
+        format_code_in_doc_comments = true;
+        format_macro_bodies = true;
+        format_macro_matchers = true;
+        format_strings = true;
+        group_imports = "StdExternalCrate";
+        hex_literal_case = "Lower";
+        imports_granularity = "Crate";
+        normalize_comments = true;
+        normalize_doc_attributes = true;
+        overflow_delimited_expr = true;
+        reorder_impl_items = true;
+        style_edition = "2024";
+        unstable_features = true;
+        use_field_init_shorthand = true;
+        use_try_shorthand = true;
+        wrap_comments = true;
+      };
     };
-    taplo.enable = true;
+
+    languages.rust.components = lib.mkOptionDefault [ "llvm-tools-preview" ];
+
+    git-hooks.hooks.clippy = {
+      enable = true;
+      packageOverrides = {
+        cargo = toolchain;
+        clippy = toolchain;
+      };
+      settings = {
+        allFeatures = true;
+        denyWarnings = true;
+      };
+    };
+
+    packages = with pkgs; [
+      bacon
+      cargo-outdated
+      cargo-llvm-cov
+      cargo-machete
+      cargo-mutants
+      cargo-nextest
+    ];
+
+    treefmt.config.programs = {
+      rustfmt = {
+        enable = true;
+        package = toolchain;
+      };
+      taplo.enable = true;
+    };
   };
 }
